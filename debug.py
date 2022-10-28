3502fe1d9d6bf7200ff623b5f2949c6012cd4223
Kennard Lim <kennardlim1220@gmail.com>
2022-10-27 18:10:48 -0700

    changed debug.py

--- debug.py
+++ debug.py
@@ -1,12 +1,15 @@
-#Backend people: write this iteratively.
-#Frontend people: write this recursively.
-def factorial(n):
-  return n
+To complete your exercise, you must first revert this file. 
+First type in `git log`. You should see something that says:
+commit 76ab22c0eec096f4a274c5e483f15aa8e8c44385 (HEAD -> debug)
+Author: Kennard Lim <kennardlim1220@gmail.com>
+Date:   Thu Oct 27 17:52:05 2022 -0700
 
-#Everyone, make the print statement print your name.
-def printName():
-  print("Put your name here")
+    Initialized debug.py
+    
+Copy the commit hashcode.
+Then type `git restore <hashcode>`
+Example: `git restore 76ab22c0eec096f4a274c5e483f15aa8e8c44385`
+Note: You will only need enough character for the hash to be uniquely identified. The first 6 should work, so:
+`git restore 76ab22c` will also usually work.
 
-if __name__ == "__main__":
-  print(factorial(5))
-  printName()
+It will not change the files. You will need to type `git restore .` to restore all the files to how they were right after this commit.
