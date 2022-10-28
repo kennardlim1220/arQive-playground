4fab0b203fe543c49cb90a8452e64e897ab7af12
Kennard Lim <kennardlim1220@gmail.com>
2022-10-27 18:16:10 -0700

    changed debug.py

--- debug.py
+++ debug.py
@@ -10,6 +10,6 @@ Copy the commit hashcode.
 Then type `git restore <hashcode>`
 Example: `git restore 76ab22c0eec096f4a274c5e483f15aa8e8c44385`
 Note: You will only need enough character for the hash to be uniquely identified. The first 6 should work, so:
-`git restore 76ab22c` will also usually work.
+`git reset 76ab22c` will also usually work.
 
 It will not change the files. You will need to type `git restore .` to restore all the files to how they were right after this commit.
