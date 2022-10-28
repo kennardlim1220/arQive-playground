6ad6c00403501cd81c8905c07929c6e5f76e9a83
Kennard Lim <kennardlim1220@gmail.com>
2022-10-27 18:17:37 -0700

    changed debug.py

--- debug.py
+++ debug.py
@@ -7,8 +7,8 @@ Date:   Thu Oct 27 17:52:05 2022 -0700
     Initialized debug.py
     
 Copy the commit hashcode.
-Then type `git restore <hashcode>`
-Example: `git restore 76ab22c0eec096f4a274c5e483f15aa8e8c44385`
+Then type `git reset <hashcode>`
+Example: `git reset 76ab22c0eec096f4a274c5e483f15aa8e8c44385`
 Note: You will only need enough character for the hash to be uniquely identified. The first 6 should work, so:
 `git reset 76ab22c` will also usually work.
 
