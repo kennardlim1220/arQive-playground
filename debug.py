76ab22c0eec096f4a274c5e483f15aa8e8c44385
Kennard Lim <kennardlim1220@gmail.com>
2022-10-27 17:52:05 -0700

    Initialized debug.py

---
+++ debug.py
@@ -0,0 +1,12 @@
+#Backend people: write this iteratively.
+#Frontend people: write this recursively.
+def factorial(n):
+  return n
+
+#Everyone, make the print statement print your name.
+def printName():
+  print("Put your name here")
+
+if __name__ == "__main__":
+  print(factorial(5))
+  printName()
